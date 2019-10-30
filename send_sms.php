2fff021518f7464240da1a0a3390aea8f27fc72a
suvink <suvin@richmun.org>
2019-10-30 15:17:14 +0530

    Added SMS script

--- send_sms.php
+++ send_sms.php
@@ -1,3 +1,27 @@
 <?php
 echo "badu wada";
+        // // //SendSMS
+        $smsText = "Payment Overdue!\nMonthly Fee:LKR 5000.00\nPlease make your payment as soon as possible to avoid penalty charges.Thank You!";
+        $user = "94771655198";
+        $password = "1357";
+        $text = urlencode($smsText);
+
+        $to = "0771655198";
+        $baseurl ="http://www.textit.biz/sendmsg";
+        $url = "$baseurl/?id=$user&pw=$password&to=$to&text=$text";
+        $ret = file($url);
+        
+        $res= explode(":",$ret[0]);
+        
+        if (trim($res[0])=="OK")
+        {
+        echo "Message Sent - ID : ".$res[1];
+        }
+        else
+        {
+        echo "Sent Failed - Error : ".$res[1];
+        }
+        
+        //Redirect
+        //header("Location: https://www.richmun.org/school_reg.html?success");
 ?>
